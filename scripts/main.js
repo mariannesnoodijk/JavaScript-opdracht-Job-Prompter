b8e6d7ce206fd0a66f27b3ebce8d36ef821a58cd
msnoodijk001 <ams.snoodijk@gmail.com>
2023-06-22 17:25:19 +0200

    opdrachten t/m 3

--- scripts/main.js
+++ scripts/main.js
@@ -72,3 +72,60 @@ const departments = {
 }
 
 console.log(departments);
+
+console.log('Opdracht 1a');
+console.log(`De afdeling Sales heeft ${departments.sales.numberOfEmployees} medewerkers`);
+console.log();
+console.log('Opdracht 1b');
+console.log(`Marketing is een leuke afdeling om te werken. ${departments.marketing.description}.`);
+console.log();
+console.log('Opdracht 1c');
+console.log(`De afdeling Customer Service heeft ${departments["customer-service"].numberOfEmployees} medewerkers`);
+console.log();
+console.log('Opdracht 1d');
+console.log(`Sales is een uitdagende afdeling om te werken als "Verkoopmanager". ${departments.sales.jobs[1].description}`);
+console.log();
+console.log();
+// console.log('Opdracht 2a, 2b en 2c');
+// const userInput = prompt('Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]');
+// switch (userInput) {
+//     case 'marketing':
+//         console.log(`Je koos ${userInput}. ${departments.marketing.description}`);
+//         break;
+//     case 'sales':
+//         console.log(`Je koos ${userInput}. ${departments.sales.description}`);
+//         break;
+//     case 'customer-service':
+//         console.log(`Je koos ${userInput}. ${departments["customer-service"].description}`);
+//         break;
+//     default:
+//         console.error('Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.');
+// }
+console.log('Opdracht 3a, 3b en 3c');
+const userInput = prompt('Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]');
+const resultInputString = prompt(`Je koos ${userInput}. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in.
+0: ${departments.marketing.jobs[0].title},
+1: ${departments.marketing.jobs[1].title},
+2: ${departments.marketing.jobs[2].title},
+3: ${departments.marketing.jobs[3].title}`);
+
+const resultInput = Number(resultInputString);
+switch (resultInput) {
+    case 0:
+        console.log(`Je koos ${departments.marketing.jobs[0].title}. Een uitdagende rol! ${departments.marketing.jobs[0].description}`);
+        break;
+    case 1:
+        console.log(`Je koos ${departments.marketing.jobs[1].title}. Een uitdagende rol! ${departments.marketing.jobs[1].description}`);
+        break;
+    case 2:
+        console.log(`Je koos ${departments.marketing.jobs[2].title}. Een uitdagende rol! ${departments.marketing.jobs[2].description}`);
+        break;
+    case 3:
+        console.log(`Je koos ${departments.marketing.jobs[3].title}. Een uitdagende rol! ${departments.marketing.jobs[3].description}`);
+        break;
+    default:
+        console.log(`Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.`)
+}
+
+// let ageStr = prompt('How old are you?');
+// let age = Number(ageStr);
